eaba54c9caf8effa46ae98c21cf7f8e87460b7fd
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2024-07-05 19:29:49 +0200

    (ft) Filtering by categories Step 1

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,36 +1,18 @@
 import "./FeaturedProducts.scss";
 import { Card } from "../Card/Card";
-import { useState, useEffect } from "react";
-import axios from 'axios';
+import useFetch from "../../hooks/useFetch";
 
 
+export const FeaturedProducts = ({ type }) => {
 
-export const FeaturedProducts = ({type}) => {
-
-  const [data, setData] = useState([]);
+const {data,loading,error} = useFetch(`/products?populate=*&[filters][type][$eq]=${type}`)
 
 //   axios.get(process.env.REACT_APP_API_URL+"/products").then(response => {
 //   console.log("Response Axios : ",response);
 // });
 
 // TO GET MY FEATURED PRODUCTS FROM MY STRAPI API (backend)
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(process.env.REACT_APP_API_URL+"/products?populate=*",
-        {
-          headers: {
-            Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
-          },
-        }
-      );
-      setData(res.data.data)
-      } catch(err) {
-        console.log(err)
-      }
-    };
-    fetchData();
-  }, [])
+  
 
   return (
     <div className="featuredProduct">
@@ -39,7 +21,11 @@ export const FeaturedProducts = ({type}) => {
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, vel facere odio fugiat dolores minus inventore laboriosam, quo nisi eos voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, dignissimos. </p>
       </div>
       <div className="bottom">
-        {data.map(item => (
+        {error
+        ? "Something went wrong on fetching products..."
+        : loading
+        ? "loading"
+        : data?.map(item => (
           <Card item={item} key={item.id}/>
         ))}
       </div>
